df329f61bf6df9d5efdefa982d47f5e2ec8f010c
unknown <vyomashukla@gmail.com>
2019-02-26 10:15:15 +0530

    first commit

---
+++ COMP308-W2019-MidTerm-Start/server/routes/books.js
@@ -0,0 +1,112 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+    res.render('books/details', {
+      title: "Add new Book",
+      books: ''
+    })
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+
+    let _book = new book({
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
+
+    book.create(_book, (err,book)=>{
+      if(err){
+        res.end(err);
+      }else{
+        res.redirect("/books");
+      }
+    })
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    let id = req.params.id;
+    book.findById(id, (err, book)=>{
+      if(err){
+        res.end(err);
+      }
+      else{
+        res.render("books/details",{
+          title: "Update Book",
+          books : book
+        })
+      }
+    })
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    let id = req.params.id;
+
+    let _book = book({
+      "_id" : id,
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
+
+    book.update({_id:id}, _book, (err, book) =>{
+      if(err){
+        res.render(err);
+      }
+      else{
+        res.redirect("/books");
+      }
+    });
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+      let id = req.params.id;
+
+      book.remove({_id:id}, (err, book)=>{
+        if(err){
+          res.end(err);
+        }
+        else{
+          res.redirect("/books");
+        }
+      })
+
+    });
+
+
+module.exports = router;
